2b8dc74ee35b691cb0295daf53e80883d66957e1
pabloApoca <pablo_mtv08@hotmail.com>
2021-11-17 17:01:05 -0300

    Se agregaron los ejercicios 3 y 4.

--- Go Bases/C1-Tarde/ejer03.go
+++ Go Bases/C1-Tarde/ejer03.go
@@ -14,6 +14,62 @@ Tip: tu código tiene que poder imprimir al menos 3 mensajes diferentes.
 */
 import "fmt"
 
+var edad, sueldo, antiguedad int
+var empleado bool
+
 func main() {
 	fmt.Println("Ejercicio 3")
+
+	edad := 23
+	antiguedad := 2
+	empleado := true
+	sueldo := 150000
+
+	if edad > 22 && antiguedad > 1 && empleado == true {
+		if sueldo >= 100000 {
+			fmt.Println("Prestamo aprobado, No se le aplicaran intereses.")
+		} else {
+			fmt.Println("Prestamo aprobado, Se le aplicaran intereses de 15% anual.")
+		}
+	} else if edad <= 22 {
+		fmt.Println("El usuario es menor de 22 años, no se puede efectuar el prestamo.")
+	} else if antiguedad <= 1 {
+		fmt.Println("El usuario tiene una antiguedad menor a 1 año, no se puede efectuar el prestamo.")
+	} else if empleado == false {
+		fmt.Println("El usuario no se encuentra empleado, no se puede efectuar el prestamo.")
+	}
+
+	//Resolucion con una funcion.
+	fmt.Println(prestamo(25, 3, 99, true))
+
+	//Resolucion con una funcion.
+	fmt.Println(prestamo(18, 3, 99, true))
+
+	//Resolucion con una funcion.
+	fmt.Println(prestamo(25, 3, 101, false))
+}
+
+func prestamo(edad int, antiguedad int, sueldo int, empleado bool) string {
+
+	if edad > 22 && antiguedad > 1 && empleado == true {
+
+		if sueldo >= 100000 {
+			return "Prestamo aprobado, No se le aplicaran intereses."
+		} else {
+			return "Prestamo aprobado, Se le aplicaran intereses de 15% anual."
+		}
+
+	} else if edad <= 22 {
+		return "El usuario es menor de 22 años, no se puede efectuar el prestamo."
+
+	} else if antiguedad <= 1 {
+		return "El usuario tiene una antiguedad menor a 1 año, no se puede efectuar el prestamo."
+
+	} else if empleado == false {
+		return "El usuario no se encuentra empleado, no se puede efectuar el prestamo."
+
+	} else {
+		return "Hay un fallo en el ingreso de datos."
+	}
+
 }
--- Go Bases/C1-Tarde/ejer04.go
+++ Go Bases/C1-Tarde/ejer04.go
@@ -10,6 +10,45 @@ Ej: 7, Julio
 */
 import "fmt"
 
+var mes int
+var meses = map[int]string{}
+
 func main() {
 	fmt.Println("Ejercicio 4")
+
+	meses[1] = "Enero"
+	meses[2] = "Febrero"
+	meses[3] = "Marzo"
+	meses[4] = "Abril"
+	meses[5] = "Mayo"
+	meses[6] = "Junio"
+	meses[7] = "Julio"
+	meses[8] = "Agosto"
+	meses[9] = "Septiembre"
+	meses[10] = "Octubre"
+	meses[11] = "Noviembre"
+	meses[12] = "Diciembre"
+
+	//fmt.Println(len(meses)) //Para saber la cantidad de meses
+	//fmt.Println(meses)
+	for indice, mes := range meses {
+		fmt.Println("Clave: ", indice, " Valor: ", mes)
+	}
+
+	//Resolucion 1
+	mes := 7
+	fmt.Println("El ", mes, " es el mes de ", meses[mes])
+
+	//Resolucion 2
+	fmt.Println(calcularMes(4))
+
+}
+
+func calcularMes(mes int) string {
+
+	var meses = map[int]string{1: "Enero", 2: "Febrero", 3: "Marzo", 4: "Abril", 5: "Mayo", 6: "Junio", 7: "Julio", 8: "Agosto", 9: "Septiembre", 10: "Octubre", 11: "Noviembre", 12: "Diciembre"}
+
+	elMes := meses[mes]
+	mensaje := "El mes de: " + elMes
+	return mensaje
 }
