74e0b4778da074fde9cf7bdf096eabefa1719589
pabloApoca <pablo_mtv08@hotmail.com>
2021-11-18 15:35:04 -0300

    Se agregaron los ejercicios 5 y 6.

--- Go Bases/C2-Mañana/ejer04.go
+++ Go Bases/C2-Mañana/ejer04.go
@@ -1,12 +1,13 @@
 package main
+
 /*
 Ejercicio 4 - Calcular estadísticas
 
-Los profesores de una universidad de Colombia necesitan calcular algunas estadísticas de calificaciones de 
+Los profesores de una universidad de Colombia necesitan calcular algunas estadísticas de calificaciones de
 los alumnos de un curso, requiriendo calcular los valores mínimo, máximo y promedio de sus calificaciones.
 
-Se solicita generar una función que indique qué tipo de cálculo se quiere realizar (mínimo, máximo o promedio) 
-y que devuelva otra función ( y un error en caso que el cálculo no esté definido) que se le puede pasar una 
+Se solicita generar una función que indique qué tipo de cálculo se quiere realizar (mínimo, máximo o promedio)
+y que devuelva otra función ( y un error en caso que el cálculo no esté definido) que se le puede pasar una
 cantidad N de enteros y devuelva el cálculo que se indicó en la función anterior
 
 Ejemplo:
@@ -16,22 +17,112 @@ const (
    promedio = "promedio"
    maximo = "maximo"
 )
- 
+
 ...
- 
+
 minFunc, err := operacion(minimo)
 promFunc, err := operacion(promedio)
 maxFunc, err := operacion(maximo)
- 
+
 ...
- 
+
 valorMinimo := minFunc(2,3,3,4,1,2,4,5)
 valorPromedio := promFunc(2,3,3,4,1,2,4,5)
 valorMaximo := maxFunc(2,3,3,4,1,2,4,5)
 
 */
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
+
+const (
+	minimo   = "minimo"
+	promedio = "promedio"
+	maximo   = "maximo"
+)
 
 func main() {
 	fmt.Println("Ejericio 4")
+
+	//Base calcular minimo
+	valorMinimo := calcularMinimo(2, 3, 3, 4, 1, 2, 4, 5)
+	fmt.Println("\nMinimo: ", valorMinimo)
+
+	//Base calcular maximo
+	valorMaximo := calcularMaximo(2, 3, 3, 4, 1, 2, 4, 5)
+	fmt.Println("Maximo: ", valorMaximo)
+
+	//Base calcular promedio
+	valorPromedio := calcularPromedio(2, 3, 3, 4, 1, 2, 4, 5)
+	fmt.Println("Promedio: ", valorPromedio)
+
+	//---------------------------------------------------------------------------------------
+
+	//Usamos la funcion operacion y le pasamos los datos de las constantes
+	minFunc, err := operacion(minimo)
+	promFunc, err := operacion(promedio)
+	maxFunc, err := operacion("maximo")
+	//controlamos por si alguna sale con error
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	//Llamos a cada metodo tomado como respuesta de la operacion para cada uno
+	valorMinimo1 := minFunc(2, 3, 3, 4, 10, 2, 4, 5)
+	valorPromedio1 := promFunc(2, 3, 3, 4, 1, 2, 4, 5)
+	valorMaximo1 := maxFunc(2, 3, 3, 4, 1, 2, 4, 5)
+
+	//Imprimimos los valores
+	fmt.Println("\nMinino de la operacion: ", valorMinimo1)
+	fmt.Println("Maximo de la operacion: ", valorMaximo1)
+	fmt.Println("Promedio de la operacion: ", valorPromedio1)
+
+}
+
+func calcularMinimo(numeros ...int) int {
+	min := 10000
+	for _, num := range numeros {
+		if num < min {
+			min = num
+		}
+	}
+	return min
+}
+
+func calcularMaximo(numeros ...int) int {
+	max := 0
+	for _, num := range numeros {
+		if num > max {
+			max = num
+		}
+	}
+	return max
+}
+
+func calcularPromedio(numeros ...int) int {
+	var promedio int
+	var total int
+	for _, num := range numeros {
+		total += num
+	}
+	promedio = (total / len(numeros))
+	return promedio
+}
+
+func operacion(calculo string) (func(...int) int, error) {
+
+	switch calculo {
+	case "minimo":
+		min := calcularMinimo
+		return min, nil
+	case "maximo":
+		max := calcularMaximo
+		return max, nil
+	case "promedio":
+		prom := calcularPromedio
+		return prom, nil
+	default:
+		return nil, errors.New("No existe calculo con ese nombre.")
+	}
 }
--- Go Bases/C2-Mañana/ejer05.go
+++ Go Bases/C2-Mañana/ejer05.go
@@ -37,8 +37,89 @@ cantidad += animalGato(8)
 
 
 */
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
+
+const (
+	perro     = "perro"
+	gato      = "gato"
+	hamser    = "Hamster"
+	tarantula = "TARANTULA"
+)
 
 func main() {
 	fmt.Println("Ejericio 5")
+
+	cPerro, err := animal(perro)
+	cGato, err := animal(gato)
+	cHamster, err := animal(hamser)
+	cTarantula, err := animal(tarantula)
+	//cPez, err := animal("pez")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	totalComidaPerro := cPerro(3)
+	totalComidaGato := cGato(5)
+	totalComidaHamster := cHamster(2)
+	totalComidaTarantula := cTarantula(7)
+	//tPez := cPez(3)
+
+	fmt.Println("\nPara los perros se necesita: ", totalComidaPerro, " kilos de comida.")
+	fmt.Println("\nPara los gatos se necesita: ", totalComidaGato, " kilos de comida.")
+	fmt.Println("\nPara los hamsters se necesita: ", totalComidaHamster, " kilos/gramos de comida.")
+	fmt.Println("\nPara las tarantulas se necesita: ", totalComidaTarantula, " kilos/gramos de comida.")
+	//fmt.Println("\nPara las tarantulas se necesita: ", tPez, " kilos/gramos de comida.")
+
+}
+
+func calcularPerro(cantidad float64) float64 { //Pasado a float64
+	var total float64
+	total = cantidad * 10
+	return total
+}
+
+func calcularGato(cantidad float64) float64 { //Pasado a float64
+	var total float64
+	total = cantidad * 5
+	return total
+}
+
+func calcularHamster(cantidad float64) float64 {
+	var total float64
+	total = cantidad * 0.250
+	return total
+}
+
+func calcularTarantura(cantidad float64) float64 {
+	var total float64
+	total = cantidad * 0.150
+	return total
+}
+
+//Funcion principal
+func animal(animal string) (func(float64) float64, error) {
+
+	switch animal {
+	case "PERRO", "perro", "Perro":
+		calcPerro := calcularPerro
+		return calcPerro, nil
+
+	case "GATO", "gato", "Gato":
+		calcGato := calcularGato
+		return calcGato, nil
+
+	case "HAMSTER", "hamster", "Hamster":
+		calcHamster := calcularHamster
+		return calcHamster, nil
+
+	case "TARANTULA", "tarantura", "Tarantula":
+		calcTarantura := calcularTarantura
+		return calcTarantura, nil
+
+	default:
+		return nil, errors.New("No tenemos ese tipo de animal.")
+	}
 }
