0df88968e77da78f41616ae92fa7c1967d2c7dfd
Pablo Martin Lopez <pablo_mtv08@hotmail.com>
2021-11-23 00:54:38 -0300

    Se agrego el ejer02.

--- Go Bases/C3-Tarde/Ejer02.go
+++ Go Bases/C3-Tarde/Ejer02.go
@@ -10,6 +10,7 @@ del programa como en las funciones.
 Se necesitan las estructuras:
 -	Usuario: Nombre, Apellido, Correo, Productos (array de productos).
 -	Producto: Nombre, precio, cantidad.
+
 Se requieren las funciones:
 -	Nuevo producto: recibe nombre y precio, y retorna un producto.
 -	Agregar producto: recibe usuario, producto y cantidad, no retorna nada, agrega el producto al usuario.
@@ -20,7 +21,72 @@ import (
 	"fmt"
 )
 
+type producto struct {
+	nombre   string
+	precio   float64
+	cantidad int
+}
+
+type usuario struct {
+	nombre    string
+	apellido  string
+	correo    string
+	productos []producto
+}
+
 func main() {
 	fmt.Println("Ejercicio 2")
 
+	var listaDeProductos []producto
+	p1 := producto{
+		nombre:   "Memoria ram",
+		precio:   14500,
+		cantidad: 2,
+	}
+	p2 := producto{
+		nombre:   "Disco ssd",
+		precio:   12500,
+		cantidad: 1,
+	}
+	p3 := producto{
+		nombre:   "Disco rigido",
+		precio:   7000,
+		cantidad: 1,
+	}
+
+	listaDeProductos = append(listaDeProductos, p1, p2, p3)
+
+	user1 := usuario{
+		nombre:    "Pablo",
+		apellido:  "Lopez",
+		correo:    "Pablo@gmail.com",
+		productos: listaDeProductos,
+	}
+
+	fmt.Println("Usuario numero 1:")
+	fmt.Println(user1)
+
+}
+
+//-	Nuevo producto: recibe nombre y precio, y retorna un producto.
+func nuevoProducto(nombre string, precio float64) producto {
+	p := producto{
+		nombre:   nombre,
+		precio:   precio,
+		cantidad: 1,
+	}
+	return p
+}
+
+//-	Agregar producto: recibe usuario, producto y cantidad, no retorna nada, agrega el producto al usuario.
+func agregarProducto(user usuario, prod producto, cant int) {
+
+}
+
+//-	Borrar productos: recibe un usuario, borra los productos del usuario.
+func borrarProducto(user usuario) {
+
+	/*for i, prod := range user.productos {
+		delete(i,prod)
+	}*/
 }
