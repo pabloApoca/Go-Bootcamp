273b8eaaa5a46e4905092660b021743198fb7eb8
pabloApoca <pablo_mtv08@hotmail.com>
2021-11-29 15:48:13 -0300

    Se agregaron las bases para los ejercicios de Go Web.

---
+++ Go Web/C1-Manana/main.go
@@ -0,0 +1,63 @@
+package main
+
+/*
+Para utilizar Gin se requiere la versión 1.13+ de Go, una vez instalada, utilizamos el siguiente
+comando para instalar Gin:
+go get -u github.com/gin-gonic/gin
+
+Luego lo importamos  a nuestro código:
+import "github.com/gin-gonic/gin"
+*/
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	fmt.Println("Ejercicio 3")
+
+	// Crea un router con gin
+	router := gin.Default()
+
+	// Captura la solicitud GET “/message
+	router.GET("/message", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello Peter 8-)",
+		})
+	})
+
+	type product struct {
+		Name      string
+		Price     int
+		Published bool
+	}
+
+	jsonData := `{"Name": "MacBook Air", "Price": 900, "Published": true}`
+
+	var p product
+
+	if err := json.Unmarshal([]byte(jsonData), &p); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(p)
+
+	router.GET("/productos", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": p,
+		})
+	})
+
+	router.Run()
+
+}
+
+//Ejecutamos en Postman
+
+//localhost:8080/message
+
+//localhost:8080/productos
